ea4a6e7a424c8c771016be3a47f2581f76020244
Laban Eilers (SimpliSafe) <laban.eilers@simplisafe.com>
2025-05-15 15:27:55 -0400

    filter lb metrics

--- exporter/ssdebugexporter/internal/otlptext/metrics.go
+++ exporter/ssdebugexporter/internal/otlptext/metrics.go
@@ -5,6 +5,7 @@ package otlptext // import "go.opentelemetry.io/collector/exporter/ssdebugexport
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"go.opentelemetry.io/collector/pdata/pcommon"
@@ -52,54 +53,84 @@ func (t textMetricsMarshaler) MarshalMetrics(md pmetric.Metrics) ([]byte, error)
 				// buf.logMetricDescriptor(metric)
 				// buf.logMetricDataPoints(metric)
 
-				logResourceAttrs := func(metricName string, age time.Duration, resourceMetrics pmetric.ResourceMetrics) {
-					header := fmt.Sprintf("metric age: #%s is %s old", metricName, age)
-					buf.logAttributesOneLine(header, rm.Resource().Attributes())
+				if !strings.HasPrefix(metric.Name(), "alb_") && !strings.HasPrefix(metric.Name(), "nlb_") {
 
-				}
+					logResourceAttrs := func(metricName string, age time.Duration, resourceMetrics pmetric.ResourceMetrics) {
+						header := fmt.Sprintf("metric age: #%s is %s old", metricName, age)
+						buf.logAttributesOneLine(header, rm.Resource().Attributes())
+					}
 
-				// Helper function to process datapoints and log age if needed
-				processDatapointAges := func(lenFn func() int, atFn func(int) interface{ Timestamp() pcommon.Timestamp }, metricName string) {
-					for l := 0; l < lenFn(); l++ {
-						datapointTime := atFn(l).Timestamp().AsTime()
-						age := now.Sub(datapointTime)
+					// Helper function to process datapoints and log age if needed
+					processDatapointAges := func(lenFn func() int, atFn func(int) interface {
+						Timestamp() pcommon.Timestamp
+						StartTimestamp() pcommon.Timestamp
+					}, metricName string) {
+						for l := 0; l < lenFn(); l++ {
+							datapointTime := atFn(l).Timestamp().AsTime()
+							age := now.Sub(datapointTime)
 
-						if age > time.Minute {
-							t.bucketOneMinute++
-							logResourceAttrs(metricName, age, rm)
-						} else if age > time.Second*30 {
-							t.bucketThirtySeconds++
-							logResourceAttrs(metricName, age, rm)
-						} else if age > time.Second*20 {
-							t.bucketTwentySeconds++
-						} else if age > time.Second*10 {
-							t.bucketTenSeconds++
-						} else if age > time.Second*5 {
-							t.bucketFiveSeconds++
-						} else if age > time.Second {
-							t.bucketOneSecond++
-						} else {
-							t.bucketTiny++
+							if age > time.Minute {
+								t.bucketOneMinute++
+								logResourceAttrs(metricName, age, rm)
+							} else if age > time.Second*30 {
+								t.bucketThirtySeconds++
+								logResourceAttrs(metricName, age, rm)
+							} else if age > time.Second*20 {
+								t.bucketTwentySeconds++
+							} else if age > time.Second*10 {
+								t.bucketTenSeconds++
+							} else if age > time.Second*5 {
+								t.bucketFiveSeconds++
+							} else if age > time.Second {
+								t.bucketOneSecond++
+							} else {
+								t.bucketTiny++
+							}
 						}
 					}
-				}
 
-				switch metric.Type() {
-				case pmetric.MetricTypeGauge:
-					dps := metric.Gauge().DataPoints()
-					processDatapointAges(dps.Len, func(i int) interface{ Timestamp() pcommon.Timestamp } { return dps.At(i) }, metric.Name())
-				case pmetric.MetricTypeSum:
-					dps := metric.Sum().DataPoints()
-					processDatapointAges(dps.Len, func(i int) interface{ Timestamp() pcommon.Timestamp } { return dps.At(i) }, metric.Name())
-				case pmetric.MetricTypeHistogram:
-					dps := metric.Histogram().DataPoints()
-					processDatapointAges(dps.Len, func(i int) interface{ Timestamp() pcommon.Timestamp } { return dps.At(i) }, metric.Name())
-				case pmetric.MetricTypeExponentialHistogram:
-					dps := metric.ExponentialHistogram().DataPoints()
-					processDatapointAges(dps.Len, func(i int) interface{ Timestamp() pcommon.Timestamp } { return dps.At(i) }, metric.Name())
-				case pmetric.MetricTypeSummary:
-					dps := metric.Summary().DataPoints()
-					processDatapointAges(dps.Len, func(i int) interface{ Timestamp() pcommon.Timestamp } { return dps.At(i) }, metric.Name())
+					switch metric.Type() {
+					case pmetric.MetricTypeGauge:
+						dps := metric.Gauge().DataPoints()
+						processDatapointAges(dps.Len, func(i int) interface {
+							Timestamp() pcommon.Timestamp
+							StartTimestamp() pcommon.Timestamp
+						} {
+							return dps.At(i)
+						}, metric.Name())
+					case pmetric.MetricTypeSum:
+						dps := metric.Sum().DataPoints()
+						processDatapointAges(dps.Len, func(i int) interface {
+							Timestamp() pcommon.Timestamp
+							StartTimestamp() pcommon.Timestamp
+						} {
+							return dps.At(i)
+						}, metric.Name())
+					case pmetric.MetricTypeHistogram:
+						dps := metric.Histogram().DataPoints()
+						processDatapointAges(dps.Len, func(i int) interface {
+							Timestamp() pcommon.Timestamp
+							StartTimestamp() pcommon.Timestamp
+						} {
+							return dps.At(i)
+						}, metric.Name())
+					case pmetric.MetricTypeExponentialHistogram:
+						dps := metric.ExponentialHistogram().DataPoints()
+						processDatapointAges(dps.Len, func(i int) interface {
+							Timestamp() pcommon.Timestamp
+							StartTimestamp() pcommon.Timestamp
+						} {
+							return dps.At(i)
+						}, metric.Name())
+					case pmetric.MetricTypeSummary:
+						dps := metric.Summary().DataPoints()
+						processDatapointAges(dps.Len, func(i int) interface {
+							Timestamp() pcommon.Timestamp
+							StartTimestamp() pcommon.Timestamp
+						} {
+							return dps.At(i)
+						}, metric.Name())
+					}
 				}
 			}
 		}
